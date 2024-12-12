ad408e310de156c85b54ac47b50c4bbbec2a8202
Manasi Gaikwad <imanasig@gmail.com>
2024-12-12 11:19:43 +0530

    Create A1_ComplexNos.cpp
    Created complex class for complex data type. Performed Addition & Multiplication of 2 complex objects & displayed them using Operator overloading.

---
+++ A1_ComplexNos.cpp
@@ -0,0 +1,88 @@
+// Implement a class Complex which represents the Complex Number data type. Implement the following
+//  1. Constructor (including a default constructor which creates the complex number 0+0i).
+//  2. Overloadoperator+ to add two complex numbers.
+//  3. Overload operator* to multiply two complex numbers.
+//  4. Overload operators << and >> to print and read Complex Numbers
+
+#include <iostream> 
+using namespace std; 
+ 
+class complex { 
+private: 
+    int real; 
+    int imag; 
+ 
+public: 
+    // Default constructor 
+    complex() {}; 
+ 
+    // Member functions 
+    void setvalue(int, int); 
+    void display(); 
+     
+    // Operator overloading 
+    complex operator* (complex &b); 
+    complex operator+ (complex &d); 
+ 
+    // Friend functions 
+    friend void operator <<(ostream &output, complex &s); 
+    friend void operator >>(istream &input, complex &q); 
+}; 
+ 
+// Member function definitions 
+void complex::setvalue(int c, int d) { 
+    real = c; 
+    imag = d; 
+} 
+ 
+void complex::display() { 
+    cout << real << "+" << imag << "i" << "\n"; 
+} 
+ 
+// Friend function definitions 
+void operator<<(ostream &output, complex &s) { 
+    output << s.real << "+" << s.imag << "i"; 
+} 
+ 
+void operator>>(istream &input, complex &q) { 
+    input >> q.real >> q.imag; 
+} 
+ 
+// Operator overloading definitions 
+complex complex::operator*(complex &b) { 
+    complex c7; 
+    c7.real = real * b.real; 
+    c7.imag = imag * b.imag; 
+    return c7; 
+} 
+ 
+complex complex::operator+(complex &d) { 
+    complex c8; 
+    c8.real = real + d.real; 
+    c8.imag = imag + d.imag; 
+    return c8; 
+} 
+ 
+int main() { 
+    complex c1, c2, c3, c4; 
+ 
+    // Input first complex number 
+    cout << "Enter the real & imaginary parts of the First Complex Number: "; 
+    cin >> c3; 
+    cout << "Complex Object of first is:" << c3; 
+ 
+    // Input second complex number 
+    cout << "\n\nEnter the real & imaginary parts of the Second Complex Number: "; 
+    cin >> c4; 
+    cout << "Complex Object of second is:" << c4; 
+ 
+    // Perform addition and multiplication 
+    c1 = c3 + c4; 
+    c2 = c3 * c4; 
+ 
+    // Output results 
+    cout << "\n\n Results are: \n Addition is: " << c1; 
+    cout << "\n Multiplication is: " << c2; 
+ 
+    return 0; 
+}
