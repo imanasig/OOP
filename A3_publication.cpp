fdcf38127807e817e8a7cb9da3acef30e3748261
Manasi Gaikwad <imanasig@gmail.com>
2024-12-17 11:49:00 +0530

    Update A3_publication.cpp

--- A3_publication.cpp
+++ A3_publication.cpp
@@ -1,7 +1,7 @@
-Imagine a publishing company which does marketing for book and audio cassette versions.
-Create a class publication that stores the title (a string) and price (type float) of publications. From this class derive two classes:
-book which adds a page count (type int) and tape which adds a playing time in minutes (type float). 
-Write a program that instantiates the book and tape class, allows user to enter data and displays the data members. If an exception is caught, replace all the data member values with zero values.
+// Imagine a publishing company which does marketing for book and audio cassette versions.
+// Create a class publication that stores the title (a string) and price (type float) of publications. From this class derive two classes:
+// book which adds a page count (type int) and tape which adds a playing time in minutes (type float). 
+// Write a program that instantiates the book and tape class, allows user to enter data and displays the data members. If an exception is caught, replace all the data member values with zero values.
 
 #include<iostream> 
 using namespace std; 
@@ -112,6 +112,7 @@ int main() {
                 } 
                 break; 
             case 5: 
+                cout<<"Thanks for using this program!";
                 exit(0); 
             default: 
                 cout<<"Enter valid choice!";
